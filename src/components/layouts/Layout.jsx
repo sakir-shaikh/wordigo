606ac30c7af4e2c0f4759db88dad1c92f754c5db
shaikh-sakir <sakir-shaikh>
2025-06-19 23:14:01 +0530

    add fontawesome, and include header and footer in layout.

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -1,14 +1,21 @@
-
 function Layout(prop) {
-    console.log(prop);
-    const { children } = prop;
+  console.log(prop);
+  const { children } = prop;
   return (
-      <>
-          <header>header</header>
-          {children}
-          <footer>footer</footer>
+    <>
+      <header>
+        <h1 className="text-gradient">Wordigo</h1>
+      </header>
+      {children}
+      <footer>
+        <small>created by</small>
+        <a target="_blank" href="https://github.com/sakir-shaikh">
+          <p>@sakir-shaikh</p>
+          <i className="fa-brands fa-github"></i>
+        </a>
+      </footer>
     </>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
