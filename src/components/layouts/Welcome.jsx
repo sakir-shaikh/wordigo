64f8255feed5fa05f4cfcf113b4a5241264cafcc
shaikh-sakir <sakir-shaikh>
2025-06-21 12:07:58 +0530

    create the welcome page with styling

--- src/components/layouts/Welcome.jsx
+++ src/components/layouts/Welcome.jsx
@@ -1,5 +1,24 @@
 function Welcome() {
-  return <div>welcome</div>;
+  return (
+    <section id="welcome">
+      <h3 className="text-large special-shadow">
+        365 Days. <br />
+        365 Words.
+      </h3>
+
+      <h6>
+        Build your dictionary
+        <br />
+        start Challenge today!
+      </h6>
+      <div id="input">
+        <input type="text" placeholder="wirte your name..." />
+        <button>
+          <h6>start &rarr;</h6>
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default Welcome;
