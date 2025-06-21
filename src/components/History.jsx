0edd3965cf200aec5c2242aa0ee0b3bd0eeaa41a
shaikh-sakir <sakir-shaikh>
2025-06-21 14:39:18 +0530

    setup history card.

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,5 +1,24 @@
 function History() {
-  return <div>History</div>;
+  return (
+    <div className="card history-card">
+      <h4>History</h4>
+      <p>
+        you have no attempts press <b>start</b> to begin 🏍
+      </p>
+      <div className="history-list">
+        <div className="card-button-secondary">
+          <div>
+            <p>started</p>
+            <h6>14/02/22</h6>
+          </div>
+          <div>
+            <p>streak 🔥 </p>
+            <h6>{4} days</h6>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default History;
