585b65e237f46b4be51451287d26ebceadd2d1e0
shaikh-sakir <sakir-shaikh>
2025-06-21 13:33:43 +0530

    Structure the coundown & did some style changes

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,5 +1,25 @@
 function Countdown() {
-  return <div>Countdown</div>;
+  return (
+    <section id="countdown">
+      <div className="card countdown-card">
+        <h1 className="item-header">Day {16}</h1>
+        <div className="today-container">
+          <div>
+            <p>Time remaining</p>
+            <h3>{"23H 59M 59S"}</h3>
+          </div>
+          <div>
+            <p>Words for today</p>
+            <h3>{16}</h3>
+          </div>
+        </div>
+
+        <button className="start-task">
+          <h6>Start</h6>
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default Countdown;
