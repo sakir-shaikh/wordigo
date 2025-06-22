b831dc3479c78fb49466aafeb89138ed9eb2844d
shaikh-sakir <sakir-shaikh>
2025-06-22 11:59:30 +0530

    countdown is up and running and did some minor fixation

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ import Dashboard from "./components/layouts/Dashboard";
 import Layout from "./components/layouts/Layout";
 import Welcome from "./components/layouts/Welcome";
 import WORDS from "./data/VOCAB.json";
-import { getWordByIndex, PLAN } from "./utils/index.js";
+import { countdownIn24Hours, getWordByIndex, PLAN } from "./utils/index.js";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState(0);
   const [name, setName] = useState("");
   const [day, setDay] = useState(1);
   const [datetime, setDatetime] = useState(null);
-  const [history, setHistory] = useState([]);
+  const [history, setHistory] = useState({});
   const [attempts, setAttempts] = useState(0);
   useEffect(() => {
     if (!localStorage) {
@@ -28,6 +28,58 @@ function App() {
       setAttempts((prevAttempts) => {
         return parseInt(localStorage.getItem("attempts"));
       });
+    }
+
+    //checking for history
+    if (localStorage.getItem("history")) {
+      setHistory((prevHistory) => {
+        return localStorage.getItem("history");
+      });
+    }
+
+    //cheking day & datetime.
+
+    if (localStorage.getItem("day")) {
+      //the consisit of an object that contains the key day and datetime.
+      const { day: d, datetime: dt } = JSON.parse(localStorage.getItem("day"));
+      setDay((prevDay) => {
+        return d;
+      });
+      setDatetime((prevDatetime) => {
+        return dt;
+      });
+    }
+
+    if (localStorage.getItem("day")) {
+      const { day: d, datetime: dt } = JSON.parse(localStorage.getItem("day"));
+      setDatetime(dt);
+      setDay(d);
+
+      if (d > 1 && dt) {
+        const diff = countdownIn24Hours(dt) ;
+        if (diff < 0) {
+          console.log("Failed challenge");
+          let newHistory = { ...history };
+          const timestamp = new Date(dt);
+          const formattedTimestamp = timestamp
+            .toString()
+            .split(" ")
+            .slice(1, 4)
+            .join(" ");
+          newHistory[formattedTimestamp] = d;
+          setHistory(newHistory);
+          setDay(1);
+          setDatetime(null);
+          setAttempts(0);
+
+          localStorage.setItem("attempts", 0);
+          localStorage.setItem("history", JSON.stringify(newHistory));
+          localStorage.setItem(
+            "day",
+            JSON.stringify({ day: 1, datetime: null })
+          );
+        }
+      }
     }
   }, []);
 
--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,47 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { convertMilliseconds, countdownIn24Hours } from "../utils";
-function Countdown(props) {
-  const { dayWords, handleChangePage, datetime, day } = props;
+
+export default function Countdown(props) {
+  const { handleChangePage, dayWords, datetime, day } = props;
+
   const targetMillis = datetime || Date.UTC(1944, 2, 17, 12, 0, 0);
   const [remainingMs, setRemainingMs] = useState(
     countdownIn24Hours(targetMillis)
   );
+
   const timer = convertMilliseconds(remainingMs);
 
-  console.log(timer);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingMs(countdownIn24Hours(targetMillis));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [targetMillis]);
 
   return (
-    <section id="countdown">
-      <div className="card countdown-card">
-        <h1 className="item-header">Day {16}</h1>
-        <div className="today-container">
-          <div>
-            <p>Time remaining</p>
-            <h3>
-              {datetime
-                ? `${Math.abs(timer.hours)}H 
-                ${Math.abs(timer.minutes)}M
-                ${Math.abs(timer.seconds)}S`
-                : "23H : 59M : 59S"}
-            </h3>
-          </div>
-          <div>
-            <p>Words for today</p>
-            <h3>{dayWords.length}</h3>
-          </div>
+    <div className="card countdown-card">
+      <h1 className="item-header">Day {day}</h1>
+      <div className="today-container">
+        <div>
+          <p>Time remaining</p>
+          <h3>
+            {datetime
+              ? `${Math.abs(timer.hours)}H ${Math.abs(
+                  timer.minutes
+                )}M ${Math.abs(timer.seconds)}S`
+              : "23H 59M 59S"}
+          </h3>
         </div>
-
-        <button
-          className="start-task"
-          onClick={() => {
-            handleChangePage(2);
-          }}
-        >
-          <h6>Start</h6>
-        </button>
+        <div>
+          <p>Words for today</p>
+          <h3>{dayWords.length}</h3>
+        </div>
       </div>
-    </section>
+
+      <button
+        onClick={() => {
+          handleChangePage(2);
+        }}
+        className="start-task"
+      >
+        <h6>Start</h6>
+      </button>
+    </div>
   );
 }
-
-export default Countdown;
