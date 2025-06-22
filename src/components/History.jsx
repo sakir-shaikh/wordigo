c6dda89cb840211724459e992a9d61dac0002de3
shaikh-sakir <sakir-shaikh>
2025-06-22 12:12:25 +0530

    corrected the history page.

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,7 +1,7 @@
 export default function History(props) {
   const { history } = props;
   const historyKeys = Object.keys(history);
-  console.log(historyKeys);
+
   return (
     <div className="card history-card">
       <h4>History</h4>
@@ -11,14 +11,14 @@ export default function History(props) {
         </p>
       ) : (
         <div className="history-list">
-          {historyKeys.map((item, _index) => {
+          {historyKeys.map((item, itemIdx) => {
             const dateKey = new Date(item)
               .toString()
               .split(" ")
               .slice(1, 4)
-              .join(" "); 
+              .join(" ");
             return (
-              <div key={_index} className="card-button-secondary">
+              <div key={itemIdx} className="card-button-secondary">
                 <div>
                   <p>Started</p>
                   <h6>{dateKey}</h6>
